902706f63caa6fa75afbfa186ec9984a3cdacf17
Herma <49307226+hermaplusplus@users.noreply.github.com>
2022-05-28 04:20:58 +0100

    Adds non-stdout logging

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import discord
+import logging
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='vergil.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 intents = discord.Intents.default()
 intents.messages = True
