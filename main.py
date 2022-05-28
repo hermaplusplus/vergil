9e7b31a7f10272b4652ac9ef8820b797f0eccc3b
Herma <49307226+hermaplusplus@users.noreply.github.com>
2022-05-28 05:58:05 +0100

    Logging refactor

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import discord
 import logging
+import os
 from random import choice as choose
+from datetime import datetime
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='vergil.log', encoding='utf-8', mode='w')
+if not os.path.exists("logs"):
+    os.makedirs("logs")
+handler = logging.FileHandler(filename='logs/vergil-' + datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + '.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
